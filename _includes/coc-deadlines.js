9b54c2e339508d12a94699027cdd050aa2a84a5d
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-07-15 01:11:15 +0200

    use block elements in general
    for submission deadlines

--- _includes/coc-deadlines.js
+++ _includes/coc-deadlines.js
@@ -4,14 +4,10 @@
 {
   title  : 'Submission deadline',
   url    : "{{ '/programme/call-for-contributions/' | relative_url }}",
-  {% if include.background %}
   display: "block",
   color  : "#d8b117",
   textColor: "#740B70",
   classNames: ["fg-coc-deadline"],  // to change the font-weight
-  {% else %}
-  color  : "#d8b117",
-  {% endif %}
   start  : "{{ deadline_start | date_to_xmlschema }}",
   end    : "{{ deadline_end | date_to_xmlschema }}"
 }{% unless forloop.last %},{% endunless %}
