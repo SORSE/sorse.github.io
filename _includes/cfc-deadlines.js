841f552739c6581f1425dba6eb27b27faa2c6947
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-07-17 11:28:27 +0200

    corrrected cfc abbreviation
    previously I named it coc (which is not the abbreviation for "call for contributions")

--- _includes/cfc-deadlines.js
+++ _includes/cfc-deadlines.js
@@ -1,4 +1,4 @@
-{%- for deadline in site.data.coc_deadlines -%}
+{%- for deadline in site.data.cfc_deadlines -%}
 {%- capture deadline_start -%}{{ deadline }}T23:00:00-0100{%- endcapture -%}
 {%- capture deadline_end -%}{{ deadline }}T23:00:00-0100{%- endcapture -%}
 {
@@ -7,7 +7,7 @@
   display: "block",
   color  : "#d8b117",
   textColor: "#740B70",
-  classNames: ["fg-coc-deadline"],  // to change the font-weight
+  classNames: ["fg-cfc-deadline"],  // to change the font-weight
   start  : "{{ deadline_start | date_to_xmlschema }}",
   end    : "{{ deadline_end | date_to_xmlschema }}"
 }{% unless forloop.last %},{% endunless %}
