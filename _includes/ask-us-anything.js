366ff0dba7ae90cef620fef3e4ff13a28b863c5a
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-07-23 21:12:26 +0200

    add onclick to ask-us-anything.js

--- _includes/ask-us-anything.js
+++ _includes/ask-us-anything.js
@@ -1,7 +1,11 @@
 {% for time in site.data.ask_us_anything %}
 {
   title: "Ask us anything",
+  {%- if include.onclick == 'tag' %}
+  url: "#ask-us-anything-sessions",
+  {%- else %}
   url: "{{ '/programme/ask-us-anything/' | relative_url }}",
+  {%- endif %}
   display: "block",
   color: "#A05F9D",
   start: '{{ time }}',
