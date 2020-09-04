e8e2898173ef418d33c64482ae5fee8831b4c525
Eileen Kuehn <eileen.kuehn@kit.edu>
2020-09-04 11:56:47 +0200

    button defaults to visible if no timestamp is found

--- _includes/hide-data.js
+++ _includes/hide-data.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hiding_elements = document.querySelectorAll("[data-hide-at]")
     for (const element of hiding_elements) {
-        if (element.getAttribute("data-hide-at") < Date.now()) {
+        const timestamp = element.getAttribute("data-hide-at")
+        if (timestamp && timestamp < Date.now()) {
             element.style.display = "none";
         }
     }
