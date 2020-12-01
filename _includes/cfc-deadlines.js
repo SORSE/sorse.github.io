e76586d60d6004c5c4da9faea516327ea11e2773
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-12-01 15:25:39 +0100

    add custom tag for lightning talk session
    as this is necessary for the full event calendar

--- _includes/cfc-deadlines.js
+++ _includes/cfc-deadlines.js
@@ -5,12 +5,18 @@
 {%- capture deadline_end -%}{{ deadline }}T23:00:00-0100{%- endcapture -%}
 {
   title: '{{ attrs.title | default: "Submission deadline"}}',
+  {%- if include.onclick == 'tag' -%}
+  {%- if attrs.tag %}
+  url    : "#{{ attrs.tag }}",
+  {%- else %}
+  url: "#submission-deadlines",
+  {%- endif -%}
+  {%- else -%}
   {%- if attrs.url %}
-  url    : "{{ attrs.url | relative_url }}",
-  {%- else if include.onclick == 'tag' %}
-  url    : "#submission-deadlines",
+    url: "{{ attrs.url | relative_url }}",
   {%- else %}
   url    : "{{ '/faq/howto/submission-deadlines' | relative_url }}",
+  {%- endif -%}
   {%- endif %}
   display: "block",
   color  : "#d8b117",
